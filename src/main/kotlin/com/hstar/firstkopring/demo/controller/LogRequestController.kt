86d2b67a739d31bddb3da2dfe8e4a41e1969d8fd
sjnam0108 <sjnam@bbmc.co.kr>
2023-08-08 15:27:52 +0900

    로그 업데이트

--- src/main/kotlin/com/hstar/firstkopring/demo/controller/LogRequestController.kt
+++ src/main/kotlin/com/hstar/firstkopring/demo/controller/LogRequestController.kt
@@ -2,16 +2,19 @@ package com.hstar.firstkopring.demo.controller
 
 import com.hstar.firstkopring.demo.dto.LogRequestDto
 import com.hstar.firstkopring.demo.service.LogRequestService
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-
+@Slf4j
 @RestController
 class LogRequestController @Autowired constructor(val logRequestService: LogRequestService) {
-
+    val logger: Logger = LoggerFactory.getLogger(LogRequestController::class.java)
     @ResponseBody
     @PostMapping("/store/api/sendLog")
     fun addLog(@RequestBody logRequestDto: LogRequestDto): ResponseEntity<Any> {
@@ -19,6 +22,7 @@ class LogRequestController @Autowired constructor(val logRequestService: LogRequ
         val save = logRequestService.save(logRequestDto)
         val res = ResResult(200, "OK", save != 0L)
         return ResponseEntity.ok().body(res)
+        logger.info("바보")
     }
 
 }
